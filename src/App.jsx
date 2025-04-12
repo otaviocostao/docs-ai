7edbbb490662675b243b481b97dc63bd9fccde78
otaviocostao <dev.otaviocostao@gmail.com>
2025-04-12 18:45:00 -0300

    feature/Componentes da barra de navegação e apresentação do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import InfoArea from './components/InfoArea/InfoArea'
+import NavBar from './components/navbar/NavBar'
 
 function App() {
 
   return (
     <>
+    <NavBar />
+    <InfoArea />
     </>
   )
 }
