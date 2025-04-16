8578e1b7f1fd304760b05146cc4efd223c3bcec7
otaviocostao <dev.otaviocostao@gmail.com>
2025-04-15 21:00:17 -0300

    Backend Java do projeto e .gitignore unificado

---
+++ backend/src/main/java/com/otavio/docsai/DocsaiApplication.java
@@ -0,0 +1,13 @@
+package com.otavio.docsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/otavio/docsai/DocsaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otavio.docsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
